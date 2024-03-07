50aa0577b646aad7eb23ad9f147b5d23dfcc9349
USER <USER@DESKTOP-MVFTAB2>
2024-03-07 11:05:09 +0900

    240307/1104/호영/로그인컨트롤러작성

--- pawsome/src/main/java/com/soa/controller/LoginController.java
+++ pawsome/src/main/java/com/soa/controller/LoginController.java
@@ -5,12 +5,36 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.soa.model.Member;
+import com.soa.model.MemberDAO;
+
 
 public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 		protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			request.setCharacterEncoding("UTF-8");
 			
+			String id = request.getParameter("id");
+			String pw = request.getParameter("pw");
+			
+			
+			Member member = new Member(id, pw);
+
+			MemberDAO dao = new MemberDAO();
+			Member res = dao.login(member);
+			
+			if (res != null) {
+				System.out.println(" 로그인 성공");
+				HttpSession session = request.getSession();
+				session.setAttribute("member", res);
+				response.sendRedirect("");
+			} else {
+				System.out.println("로그인 실패");
+				response.sendRedirect("");
+			}
 	}
 
 }
