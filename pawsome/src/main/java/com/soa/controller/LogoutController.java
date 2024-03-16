e0250923e138ab58e13009ea312d3cc444dc006a
USER <USER@DESKTOP-MVFTAB2>
2024-03-16 10:49:05 +0900

    240316/1048/양호영/로그아웃컨트롤러 수정

--- pawsome/src/main/java/com/soa/controller/LogoutController.java
+++ pawsome/src/main/java/com/soa/controller/LogoutController.java
@@ -10,10 +10,12 @@ import javax.servlet.http.HttpSession;
 public class LogoutController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-		protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-			HttpSession session = request.getSession();
-			session.invalidate();
-			response.sendRedirect("");
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		HttpSession session = request.getSession();
+		session.invalidate();
+		response.sendRedirect("main.jsp");
 	}
 
 }
